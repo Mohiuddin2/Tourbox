02c48814dd1cb48cd7e5596f3adcf113af125392
Mohiuddin2 <mohibdrs@gmail.com>
2021-11-29 14:15:05 +0600

    Before Final

--- app.js
+++ app.js
@@ -1,156 +1,180 @@
-// Dev loggin middleware --want to run into development env
-// if (process.env.NODE_ENV === "development") {
-//   app.use(morgan("dev"));
+// if (process.env.NODE_ENV !== "production") {
+//   require('dotenv').config();
 // }
+require('dotenv').config();
 
-// console.log(process.env.Api_Key)
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
 
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const session = require("express-session");
-const flash = require("connect-flash");
-const MongodbSession = require("connect-mongodb-session")(session);
-const path = require("path");
-const ejsMate = require("ejs-mate");
-// const { campgroundSchema, reviewSchema } = require("./schemas.js"); // Joi Server side validation
-const catchAsync = require("./utility/catchAsync");
-const ExpressError = require("./utility/ExpressError");
-const methodOverride = require("method-override");
-const User = require("./models/user");
-const { expression } = require("joi");
-const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const dotenv = require("dotenv").config();
-const morgan = require("morgan"); //Logger
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+
+
+const ExpressError = require('./utility/ExpressError');
+const methodOverride = require('method-override');
+const passport = require('passport');
+
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+const helmet = require('helmet');
+
 const mongoSanitize = require('express-mongo-sanitize');
-//Load env vars
-// dotenv.config({ path: "./config/config.env" });
+const userRoutes = require('./routes/userRoute');
+const campgroundRoutes = require('./routes/campground'); // model
+const reviewRoutes = require('./routes/reviews'); //model
 
-// mongo securiy db security for restricting $
-app.use(mongoSanitize({
-  replaceWith: '_',
-}));
+const MongoStore = require('connect-mongo');
+// const MongoDBStore = require("connect-mongo")(session);
 
-const campgroundsRoute = require("./routes/campground");
-const reviewsRoute = require("./routes/reviews");
-const userRoute = require("./routes/userRoute");
+const dbUrl = process.env.MongoURL || 'mongodb://localhost:27017/yelp-camp';
 
-// For connect flash must follow this secuencial order-- accor. to doc pass.. initialize will be after session as bellow
-app.use(
-  session({
-    secret: "YelcampSecret",
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      httpOnly: true,
-      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-      maxAge: 1000 * 60 * 60 * 24 * 7,
-    },
-  })
-);
-// Connect-flash must follow order-- before router and flas.. app use
-app.use(flash());
-// passport will be here--
-app.use(passport.initialize());
-app.use(passport.session());
-// passport.use(new LocalStrategy(User.authenticate()))
-
-passport.use(
-  new LocalStrategy(
-    {
-      usernameField: "username",
-      passwordField: "password",
-    },
-    User.authenticate()
-  )
-);
-
-passport.serializeUser(User.serializeUser()); // for how do we store user in the session
-passport.deserializeUser(User.deserializeUser()); // for removing user form session
-
-app.use((req, res, next) => {
-  console.log(req.query)
-  res.locals.currentUser = req.user, // this for session logged in or for
-  res.locals.success = req.flash("success");
-  res.locals.error = req.flash("error");
-  next();
-});
-//
-const mongoURI = "mongodb://localhost:27017/yelpcamp";
-// connect mongoose
-mongoose.connect(mongoURI, {
+mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "Connection error:"));
+db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-  console.log("Database Connected");
+  console.log("Database connected");
 });
 
-// mongodb session
-const store = new MongodbSession({
-  uri: mongoURI,
-  collection: "mySession",
-});
+const app = express();
 
-//to execute folowing
-app.engine("ejs", ejsMate);
-app.set("view engine", "ejs"); // setting ejs view engine
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "public")));
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize({
+  replaceWith: '_'
+}))
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
 
-// Router
-app.use("/", userRoute);
-app.use("/campgrounds", campgroundsRoute);
-app.use("/campgrounds/:id/reviews", reviewsRoute);
+// const store = new MongoDBStore({
+//   url: dbUrl,
+//   secret,
+//   touchAfter: 24 * 60 * 60
+// });
 
-const sessionconfig = {
-  secret: "High_secrete",
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  touchAfter: 24 * 2300,
+  secret, 
+})
+
+
+store.on("error", function (e) {
+  console.log("SESSION STORE ERROR", e)
+})
+
+const sessionConfig = {
+  store,
+  name: 'session',
+  secret,
   resave: false,
-  saveUninitialized: false,
-  store: store,
-  // cookie: { secure: true }
-};
-app.use(session(sessionconfig));
-// Middleware for session
-const isAuth = (req, res, next) => {
-  if (req.session.isAuth) {
-    next();
-  } else {
-    res.redirect("/login");
+  saveUninitialized: false, // this was false I made it .// don't create session until something stored
+  cookie: {
+      httpOnly: true,
+      // secure: true,
+      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+      maxAge: 1000 * 60 * 60 * 24 * 7
   }
-};
+}
 
-// Logout
-app.post("/logout", (req, res) => {
-  req.session.destroy((err) => {
-    if (err) throw err;
-    res.redirect("/login");
-  });
+app.use(session(sessionConfig));
+app.use(flash());
+app.use(helmet());
+
+
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com",
+  "https://api.tiles.mapbox.com",
+  "https://api.mapbox.com",
+  "https://kit.fontawesome.com",
+  "https://cdnjs.cloudflare.com",
+  "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+  "https://kit-free.fontawesome.com",
+  "https://stackpath.bootstrapcdn.com",
+  "https://api.mapbox.com",
+  "https://api.tiles.mapbox.com",
+  "https://fonts.googleapis.com",
+  "https://use.fontawesome.com",
+];
+const connectSrcUrls = [
+  "https://api.mapbox.com",
+  "https://*.tiles.mapbox.com",
+  "https://events.mapbox.com",
+];
+const fontSrcUrls = [];
+app.use(
+  helmet.contentSecurityPolicy({
+      directives: {
+          defaultSrc: [],
+          connectSrc: ["'self'", ...connectSrcUrls],
+          scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+          styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+          workerSrc: ["'self'", "blob:"],
+          childSrc: ["blob:"],
+          objectSrc: [],
+          imgSrc: [
+              "'self'",
+              "blob:",
+              "data:",
+              "https://res.cloudinary.com/dgbkufw6z/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+              "https://images.unsplash.com",
+              "https://images.pexels.com/"
+          ],
+          fontSrc: ["'self'", ...fontSrcUrls],
+      },
+  })
+);
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser()); // for how do we store user in the session
+passport.deserializeUser(User.deserializeUser());  // for removing user form session
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user; // this for session logged in or for
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+})
+
+
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
+
+
+app.get('/', (req, res) => {
+  res.render('home')
 });
 
-// Home
-app.get("/", (req, res) => {
-  // req.session.isAuth = true;
-  res.render("home");
-});
+
 
 // This is mystry I need add this on project
 
-// app.all("*", (req, res, next) => {
-//   next(new ExpressError("Page Not Found", 404));
-// });
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
 
-// // // default error handler..
-// app.use((err, req, res, next) => {
-//   const { statusCode = 500 } = err;
-//   if (!err.message) err.message = "Saomething Went Wrong";
-//   res.status(statusCode).render("error", { err });
-// });
+// default error handler..
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Saomething Went Wrong";
+  res.status(statusCode).render("error", { err });
+});
 
 app.listen(5000, () => {
   console.log("Serving on port 5000");
